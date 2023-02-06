59a5ea1ea4ca7b5f587a5c10f96fc99a3f269706
Anirban Samanta <ani.samanta365@outlook.com>
2023-02-06 20:29:16 +0530

    Resolved merge conflicts

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,112 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  height: 60px;
+  bg-color: #3C6255;
+  ${mobile({ height: "50px" })}
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  background-color: #3C6255;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
+  ${mobile({ display: "none" })}
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  background-color: white;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  ${mobile({ width: "50px" })}
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  background-color: #3C6255;
+  ${mobile({ fontSize: "24px" })}
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  font-weight: bold;
+  ${mobile({ fontSize: "12px",color: "white", marginLeft: "10px" })}
+`;
+
+const Navbar = () => {
+  const quantity = useSelector(state=>state.cart.quantity)
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language style={{color: "white"}}>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search Products " />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>AgriAgio.</Logo>
+        </Center>
+        <Right>
+          <Link to="/register">
+          <MenuItem style={{color: "white"}}>REGISTER</MenuItem>
+          </Link>
+          <Link to="/login">
+          <MenuItem style={{color: "white"}}>SIGN IN</MenuItem>
+          </Link>
+          <Link to="/cart">
+          <MenuItem>
+            <Badge badgeContent={quantity} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+          </Link>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
