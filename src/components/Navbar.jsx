ac4b165bdc3d865a26bad3e667c413eaf84bd24c
Subradeep10 <subhradeepsamanta2002@gmail.com>
2023-02-08 08:54:23 +0530

    Backend Added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,116 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  height: 60px;
+  bg-color: #06bdbd;
+  ${mobile({ height: "50px" })}
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  background-color: #06bdbd;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
+  ${mobile({ display: "none" })}
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  background-color: white;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  ${mobile({ width: "50px" })}
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  background-color: #06bdbd;
+  ${mobile({ fontSize: "24px" })}
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  font-weight: bold;
+  ${mobile({ fontSize: "12px",color: "white", marginLeft: "10px" })}
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language style={{color:"white"}}>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>
+            <span style={{color: "white"}}>
+            Agri
+            </span>
+            <span style={{color: "yellow"}}>
+            Agio
+            </span>
+          </Logo>
+        </Center>
+        <Right>
+          <Link to="/register">
+          <MenuItem style={{color:"white"}}>REGISTER</MenuItem>
+          </Link>
+          <Link to="/login">
+          <MenuItem style={{color:"white"}}>SIGN IN</MenuItem>
+          </Link>
+          <Link to="/cart">
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+          </Link>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+}; 
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/vjYpqgC/Tractor.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON QUALITY! GET FLAT 15% OFF ON NEW Farming Tools",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/XCcxzRk/fertilizer2.png",
+      title: "Organic Fertilizers",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW Fertilizers.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://img.freepik.com/premium-psd/organic-wild-seed-bag-mockup-transparent-plastic-poly-bag-mockup_475259-1290.jpg?w=2000",
+      title: "SUPER SEEDS",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW SEEDS",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/DD2gRr4/Agro-Seeds.png",
+      title: "AGRO SEEDS!",
+      cat:"seeds"
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/XCcxzRk/fertilizer2.png",
+      title: "FERTILIZERS & PESTICIDES",
+      cat:"fertilizers"
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/Tm2BkM8/tools.webp",
+      title: "FARMING TOOLS",
+      cat:"tools"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://img.freepik.com/premium-psd/organic-wild-seed-bag-mockup-transparent-plastic-poly-bag-mockup_475259-1290.jpg?w=2000",
+    },
+    {
+      id:2,
+      img:"https://5.imimg.com/data5/HA/DA/GN/SELLER-10295077/sorghum-seed-packaging-bag-500x500.jpg",
+    },
+    {
+      id:3,
+      img:"https://5.imimg.com/data5/HI/LU/WN/SELLER-4492648/pp-seeds-bag-500x500.jpg",
+    },
+    {
+      id:4,
+      img:"https://sc04.alicdn.com/kf/Hb2957f01eadc4dfcb253ae7a2750ba3bR/206198098/Hb2957f01eadc4dfcb253ae7a2750ba3bR.jpg",
+    },
+    {
+      id:5,
+      img:"https://m.media-amazon.com/images/I/61bkPnb6eDL._SX522_.jpg",
+    },
+    {
+      id:6,
+      img:"https://m.media-amazon.com/images/I/612l56NcHOL._SX569_.jpg",
+    },
+    {
+      id:7,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThpEiuRs17sIE4XbHWr-n5caj2RMKZyi9Q8A&usqp=CAU",
+    },
+    {
+      id:8,
+      img:"https://www.jiomart.com/images/product/original/rvcu49ty73/ponkathir-fennel-seed-400-gram-100gx4pkts-saunf-mouri-peruncheeragam-400-grams-product-images-orvcu49ty73-p595075649-0-202211051304.jpg",
+    },
+  ]
